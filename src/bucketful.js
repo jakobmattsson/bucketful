3ec11806530987586db23ea7b019af4e41dd7912
Jakob Mattsson <jakob.mattsson@gmail.com>
2012-07-18 00:28:39 +0200

    Include-directive and improved logging

--- src/bucketful.js
+++ src/bucketful.js
@@ -16,6 +16,7 @@ exports.deploy = function(options) {
   nconf.env().defaults({
     bucketful: {
       targetDir: path.join(process.cwd(), 'public'),
+      include: [],
       exclude: [],
       opra: { inline: true }
     }
@@ -25,10 +26,11 @@ exports.deploy = function(options) {
   var aws_key = nconf.get('bucketful:key');
   var aws_secret = nconf.get('bucketful:secret');
   var excludes = nconf.get('bucketful:exclude');
+  var includes = nconf.get('bucketful:include');
   var opraOptions = nconf.get('bucketful:opra');
   var targetDir = path.resolve(nconf.get('bucketful:targetDir'));
 
-  console.log("targetDir", targetDir);
+  console.log('Resolved targetDir', targetDir);
   console.log('Loaded the following options', nconf.get('bucketful'));
 
   var client = knox.createClient({
@@ -53,13 +55,32 @@ exports.deploy = function(options) {
     powerfs.writeFile('tmp/index.html', data, 'utf8', errHandler(function() {
       console.log("Uploading index.html")
       client.putFile('tmp/index.html', '/index.html', errHandler(function(res) {
-        console.log("Uploading the rest");
 
-        var files = wrench.readdirSyncRecursive(targetDir).filter(function(file) {
+        var files = [];
+
+        if (includes.length > 0) {
+          includes.forEach(function(file) {
+            var fullpath = path.join(targetDir, file);
+            if (powerfs.isDirectorySync(fullpath)) {
+              files = files.concat(wrench.readdirSyncRecursive(fullpath).map(function(f) {
+                return path.join(file, f);
+              }));
+            } else {
+              files.push(file);
+            }
+          });
+        } else {
+          files = wrench.readdirSyncRecursive(targetDir);
+        }
+
+        files = files.filter(function(file) {
           return excludes.every(function(x) {
             return file != x && !_s.startsWith(file, x + '/');
           });
         });
+
+        var counter = 0;
+        console.log("Uploading the rest (" + files.length + " files)");
 
         async.forEach(files, function(file, callback) {
           powerfs.isFile(path.join(targetDir, file), function(err, isFile) {
@@ -68,7 +89,11 @@ exports.deploy = function(options) {
             } else if (!isFile) {
               callback();
             } else {
-              client.putFile(path.join(targetDir, file), '/' + file, callback);
+              client.putFile(path.join(targetDir, file), '/' + file, function() {
+                counter++;
+                console.log("[" + counter + "/" + files.length + "] " + file);
+                callback.apply(this, arguments);
+              });
             }
           });
         }, errHandler(function() {
